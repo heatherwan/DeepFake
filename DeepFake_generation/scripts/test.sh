7261e5610b6b3b5e0ee3098e76a10b9e2f46ea6b
WantingLin <wlin@mail.uni-mannheim.de>
2020-08-01 12:38:02 +0200

    initial commit

---
+++ DeepFake_generation/scripts/test.sh
@@ -0,0 +1,2 @@
+python run_and_generate_fake_video.py -n Wanting_2min_opposite -ia Video_2_3min.mp4 -ib Video_1_3min.mp4 --audio yes --frame double 
+
---
+++ DeepFake_generation/scripts/train.sh
@@ -0,0 +1,2 @@
+python prepare_training_data_and_train.py -m cycle_gan -n Wanting_3min_test -ia Video_2_3min.mp4 -ib Video_1_3min.mp4 
+
